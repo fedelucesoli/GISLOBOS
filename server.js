2ff63125ff4467aa9ccb769507b1e8df9157522a
FedeLucesoli <fede.lucesoli@gmail.com>
2017-05-24 13:56:47 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,50 @@
+
+// Dependencies
+// -----------------------------------------------------
+var express         = require('express');
+var mongoose        = require('mongoose');
+var port            = process.env.PORT || 3000;
+var morgan          = require('morgan');
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var app             = express();
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+// Express Configuration
+// -----------------------------------------------------
+// Sets the connection to MongoDB
+//mongoose.connect("mongodb://localhost/MeanMapApp");
+
+// Logging and Parsing
+app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
+app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
+app.use(morgan('dev'));                                         // log with Morgan
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
+app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// Routes
+// ------------------------------------------------------
+// require('./app/routes.js')(app);
+
+// Listen
+// -------------------------------------------------------
+app.listen(port);
+console.log('App listening on port ' + port);
